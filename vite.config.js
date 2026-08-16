23a55a27f3b56a1d4d112bd03082eda78c827c5c
USer99pro <akbbna48@gmail.com>
2026-08-16 16:11:25 +0700

    fix: update Vite configuration for improved readability and add server port setting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: { port: 5173 },
-});
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
+  // ถ้าเว็บอยู่ที่ http://domain.com/
+  base: '/',
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+})
