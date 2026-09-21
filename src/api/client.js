ce886490b329ce4227333576501e2f5d6bf4246d
USer99pro <akbbna48@gmail.com>
2026-09-21 18:55:15 +0700

    dfdf

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { maskSensitiveData } from '../utils/security';
 
-const API_URL = (import.meta.env.VITE_API_URL).replace(/\/+$/, '');
+const API_URL = (import.meta.env.VITE_API_URL || '').replace(/\/+$/, '');
+
 let refreshRequest = null;
 
 const notifyUnauthorized = () => {
